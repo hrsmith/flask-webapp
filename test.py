f86ad8eb35431ea9fe58eb1ca680f3b8b681dbbc
hrsmith <18326645652@163.com>
2018-06-10 14:05:40 +0800

    login

--- test.py
+++ test.py
@@ -10,13 +10,24 @@ UPLOAD_FOLDER='upload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/')
-def hello_world():
-   return render_template('index.html')
 
-@app.route('/login')
-def loginAction():
+
+@app.route('/')
+def index():
     return render_template('login.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -29,6 +40,15 @@ def upload_file():
         new_filename=str(unix_time)+'.'+ext  # 修改了上传的文件名
         f.save(os.path.join(file_dir,new_filename))  #保存文件到upload目录
     return "ok"
+
+def valid_login(username,password):
+    if(username == ''or password == ''):
+        return False;
+    return True
+
+def log_the_user_in(username):
+    return "Welcome!" + username
+
 
 if __name__ == '__main__':
     app.run(debug=True)
